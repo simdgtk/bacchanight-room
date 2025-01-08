2153a8e87f589c2f8cd8a0910024186d4bdfab07
Billel T <148473122+BillelT@users.noreply.github.com>
2025-01-08 17:04:50 +0100

    scene start

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -1,7 +1,16 @@
+// Custom import
+import { Canvas } from "@react-three/fiber";
+import Experience from "../Experience/Experience.jsx";
+
 export default function Index() {
   return (
     <>
-      <h1>Bacchanight</h1>
+      <Canvas
+        camera={{ position: [0, 0, 1.5], fov: 75, near: 0.001, far: 100 }}
+        style={{ position: "fixed", top: "0", left: "0", zIndex: "0" }}
+      >
+        <Experience></Experience>
+      </Canvas>
     </>
   );
 }
